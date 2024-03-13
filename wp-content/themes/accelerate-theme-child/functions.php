182b88f1ffe8e3b7ec3e8f19a2b8a5fdc3d299a3
tauri_s <tauristclaire@gmail.com>
2024-03-12 17:31:04 -0700

    Add functions.php file to the child them with file information

---
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Accelerate Marketing Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage Accelerate Marketing
+* @since Accelerate Marketing 2.0
+*/
