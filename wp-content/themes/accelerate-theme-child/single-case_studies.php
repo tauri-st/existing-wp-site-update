8a865dfb230b99692f7cf720bae5edd2ae5e4342
tauri_s <tauristclaire@gmail.com>
2024-03-18 15:06:05 -0700

    Add variables to get all content custom fields

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -12,6 +12,12 @@ get_header(); ?>
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
+				$services = get_field('services');
+				$client = get_field('client');
+				$site_link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3');
 				<article class="case-study">
 					<aside class="case-study-sidebar">
 						<h2>MailChimp Campaign</h2>
