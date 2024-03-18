e2ae92fcf64bf59832daf9b8e84ce33ce7a9e70c
tauri_s <tauristclaire@gmail.com>
2024-03-18 16:31:50 -0700

    Add Back to work navigation bar to bottom of single case study

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -11,14 +11,14 @@ get_header(); ?>
 
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				$services = get_field('services');
-				$client = get_field('client');
-				$site_link = get_field('site_link');
-				$image_1 = get_field('image_1');
-				$image_2 = get_field('image_2');
-				$image_3 = get_field('image_3');
-				$size = "full";
+			<?php while ( have_posts() ) : the_post(); 
+				$services = get_field("services");
+				$client = get_field("client");
+				$site_link = get_field("site_link");
+				$image_1 = get_field("image_1");
+				$image_2 = get_field("image_2");
+				$image_3 = get_field("image_3");
+				$size = "full"; ?>
 				<article class="case-study">
 					<aside class="case-study-sidebar">
 						<h2><?php the_title(); ?></h2>
@@ -28,20 +28,24 @@ get_header(); ?>
 						<p class="read-more-link"><a href="<?php echo $link; ?>">Live Site</a></p>
 					</aside>
 					<div class="case-study-images">
-						<?php if ($image_1) { ?>
+						<?php if ($image_1) { 
 							echo wp_get_attachment_image( $image_1, $size );
-						<?php } ?>
-						<?php if ($image_2) { ?>
+						} ?>
+						<?php if ($image_2) {
 							echo wp_get_attachment_image( $image_2, $size );
-						<?php } ?>
-						<?php if ($image_3) { ?>
+						} ?>
+						<?php if ($image_3) {
 							echo wp_get_attachment_image( $image_3, $size );
-						<?php } ?>
+						} ?>
 					</div>
 				</article>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 
 	</div><!-- #primary -->
+
+	<nav id="navigation" class="container">
+		<div class="left"><a href="<?php echo site_url('/blog/') ?>">&larr; <span>Back to work</span></a></div>
+	</nav>
 
 <?php get_footer(); ?>
