8b36281fd37f828b8c1c666445ea93469e7b5e63
tauri_s <tauristclaire@gmail.com>
2024-03-19 17:53:15 -0700

    Remove exerpt from featured work section, wrao featured work in a ul with each featured work post pulled as an li

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,23 +25,25 @@ get_header(); ?>
 	<section class="featured-work">
 		<div class="site-content">
 			<h4>Featured Work</h4>
-
-			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
-				<?php while ( have_posts() ) : the_post();
-					$image_1 = get_field("image_1");
-					$size = "medium";
-				?>
-					<figure>
-						<?php echo wp_get_attachment_image( $image_1, $size ); ?>
-					</figure>
-					<h3>
-						<a href="<?php the_permalink(); ?>">
-							<?php the_title(); ?>
-						</a>
-					</h3>
-       				<?php the_excerpt(); ?>
-  				<?php endwhile; ?> 
-			<?php wp_reset_query(); ?>
+			<ul class="homepage-featured-work">
+				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+					<?php while ( have_posts() ) : the_post();
+						$image_1 = get_field("image_1");
+						$size = "thumbnail";
+					?>
+						<li>
+							<figure>
+								<?php echo wp_get_attachment_image( $image_1, $size ); ?>
+							</figure>
+							<h3>
+								<a href="<?php the_permalink(); ?>">
+									<?php the_title(); ?>
+								</a>
+							</h3>
+						</li>
+					<?php endwhile; ?> 
+				<?php wp_reset_query(); ?>
+			</ul>
 		</div>
 	</section>
 	<section class="recent-posts">
