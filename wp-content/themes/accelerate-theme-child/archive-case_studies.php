82f8e1eac79da0250285f18ec4456061fe5879fa
tauri_s <tauristclaire@gmail.com>
2024-03-22 15:59:11 -0700

    Copy page template into the archive, change comment info for the template, and remove the sidebar

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the case study archive
+ *
+ * It will overwrite the default archive template.
+ * The name of the template file pulls in the case study posts.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
