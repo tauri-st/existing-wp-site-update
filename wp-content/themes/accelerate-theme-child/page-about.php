49ae0b2fa5c3f4ae7518204329f546c30e4a6fae
tauri_s <tauristclaire@gmail.com>
2024-04-10 15:37:55 -0700

    Add variables for fields

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -11,7 +11,15 @@ get_header(); ?>
 
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post();
+                $service_title = get_field("services");
+                $service_description = get_field("service_description");
+				$icon_1 = get_field("icon_1");
+				$icon_2 = get_field("icon_2");
+				$icon_3 = get_field("icon_3");
+                $icon_4 = get_field("icon_4");
+                $icon_5 = get_field("icon_5");
+				$size = "medium"; ?>
                 <h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
