92eb60a1fb1df2f51eddc63b58d648cf4c75d664
tauri_s <tauristclaire@gmail.com>
2024-04-22 16:00:28 -0700

    Add custom template for 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for a 404 Site Not Found error
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+    <div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+            
+        </div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
