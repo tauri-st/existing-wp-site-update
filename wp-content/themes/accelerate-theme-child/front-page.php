e33e2292a425ad0c499d3580bd79acb11c8da77a
tauri_s <tauristclaire@gmail.com>
2024-04-24 16:14:25 -0700

    Install and activate MailChimp plugin

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -59,6 +59,11 @@ get_header(); ?>
   					<?php endwhile; ?> 
 				<?php wp_reset_query(); ?>
  			</div>
+			 <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+				<div id="secondary" class="widget-area" role="complementary">
+					<?php dynamic_sidebar( 'sidebar-2' ); ?>
+				</div>
+			<?php endif; ?>
  		</div>
 	</section>
 
